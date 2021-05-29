647d3d7578637316f82e0922cc04d24e6272c715
Leon Antoshyn <Doctorsprait@gmail.com>
2021-05-29 14:17:46 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -8,18 +8,18 @@ $name = $_POST['user_name'];
 $phone = $_POST['user_phone'];
 $theme = $_POST['user_theme'];
 
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'mail.kuzmitskaya.by';  																							// Specify main and backup SMTP servers
+$mail->Host = 'korchakovskaya.by';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'admin@kuzmitskaya.by'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'kuzmitskaya2021'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'admin@korchakovskaya.by'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'korchakovskaya2021'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('admin@kuzmitskaya.by'); // от кого будет уходить письмо?
-$mail->addAddress('juljakuzmitskaja@gmail.com');     // Кому будет уходить письмо 
+$mail->setFrom('admin@korchakovskaya.by'); // от кого будет уходить письмо?
+$mail->addAddress('ouk2007@mail.ru');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
